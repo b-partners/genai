509cb8ecba200ca37f842f71486ac92df07f67d6
Ryan Andriamahery <ryan@hei.school>
2025-07-16 10:32:09 +0300

    test: add EnvConf

---
+++ src/test/java/school/hei/hazavao/conf/EnvConf.java
@@ -0,0 +1,9 @@
+package school.hei.hazavao.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+public class EnvConf {
+  void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("openapi.key", () -> "dummy");
+  }
+}
