e910e776ed6106b0b707d34fa2ab9574bf09c98b
Daniel Langio <langio.tehiniavo@gmail.com>
2026-08-18 22:01:36 +0300

    chore(to-revert): add toiture on root path

--- src/main/java/fr/birdia/genai/endpoint/rest/controller/RoofController.java
+++ src/main/java/fr/birdia/genai/endpoint/rest/controller/RoofController.java
@@ -63,4 +63,53 @@ public class RoofController {
             hauteurBatiment,
             commentaireCouvreur));
   }
+
+  @GetMapping(value = "/", produces = TEXT_HTML_VALUE)
+  public String toitureOnRoot(
+      @RequestParam(required = false) String adresse,
+      @RequestParam(required = false) String gps,
+      @RequestParam(required = false) Integer millesimeImage1,
+      @RequestParam(required = false) Integer millesimeImage2,
+      Double surfaceEnM2,
+      @RequestParam(required = false) String typeToiture,
+      @RequestParam(required = false) Double penteMin,
+      @RequestParam(required = false) Double penteMax,
+      String revetement,
+      Double humidité,
+      Double moisissure,
+      Double usure,
+      @RequestParam(required = false) String obstacles,
+      @RequestParam(required = false) String mutation,
+      @RequestParam(required = false) Boolean fissureCassure,
+      @RequestParam(required = false) Boolean risqueFeu,
+      @RequestParam(required = false) Double noteDegradationGlobale,
+      @RequestParam(required = false) String categorie,
+      @RequestParam(required = false) String revetement2,
+      @RequestParam(required = false) Double hauteurBatiment,
+      @RequestParam(required = false) String commentaireCouvreur) {
+
+    return analyseurToiture.apply(
+        new Toit(
+            adresse,
+            gps,
+            millesimeImage1,
+            millesimeImage2,
+            surfaceEnM2,
+            typeToiture,
+            penteMin,
+            penteMax,
+            revetement,
+            humidité,
+            moisissure,
+            usure,
+            obstacles,
+            mutation,
+            fissureCassure,
+            risqueFeu,
+            noteDegradationGlobale,
+            categorie,
+            revetement2,
+            hauteurBatiment,
+            commentaireCouvreur));
+  }
 }
